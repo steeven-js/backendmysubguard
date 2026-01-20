f4d3c3b153c0ff5f9fb0dc74a5f6d73845e005de
steeven-js <jacques.steeven@gmail.com>
2026-01-20 14:14:01 -0400

    feat: ajout temporaire de la registration Filament pour créer compte admin en prod

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -31,6 +31,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login(Login::class)
+            ->registration() // TODO: Supprimer après création du compte admin
             ->colors([
                 'primary' => Color::Blue,
             ])
